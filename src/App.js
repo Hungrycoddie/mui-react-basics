499829a44ceb7c1480a06614a7dd8596d0d25388
Ar <106869558+Hungrycoddie@users.noreply.github.com>
2023-09-23 23:35:24 +0530

    mui basics
    https://www.youtube.com/watch?v=fzxEECHnsvU

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Typography, styled } from "@mui/material";
+import { Add, Settings } from "@mui/icons-material";
+
+
+
+const Bluebutton = styled(Button) (({ theme}) => ({
+  backgroundColor: "skyblue",
+  color:'#888',
+  margin: 5,
+  "&:hover": {
+    backgroundColor: "lightblue",
+  },
+  "&:disabled": {
+    backgroundColor: "gray",
+    color: "white",
+  },
+}));
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="text">Text</Button>
+
+      <Button
+        startIcon={<Settings />}
+        variant="contained"
+        color="secondary"
+        size="small">
+        Settings
+      </Button>
+
+      <Button
+        startIcon={<Add/>}
+        variant="contained"
+        color="success"
+        size="small"
+      >
+        Add new post
+      </Button>
+
+      <Button
+        variant="outlined"
+        disabled>
+        Outlined
+      </Button>
+
+      <Typography variant="h1" component="p">
+        This is a p tag in h1 style
+      </Typography>
+
+      <Bluebutton>bluebutton</Bluebutton>
+      <Bluebutton>Hello </Bluebutton>
+      <Bluebutton>Submit</Bluebutton>
+      <Bluebutton>click me</Bluebutton>
+
     </div>
   );
 }
